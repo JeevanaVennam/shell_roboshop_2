a97b2730f70abf9221e7cfe2aff908b8ba9f79f6
jeevanavennam@outlook.com <jeevanavennam@outlook.com>
2026-06-08 16:39:31 +0530

    shellroboshop

---
+++ catalogue.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+source ./common.sh
+
+app_name=$catalogue
+
+check_root
+
+app_setup
+
+nodejs_setup
+
+systemd_setup
+
+
+cp $script_dir/mongo.repo /etc/yum.repos.d/mongo.repo
+
+dnf install mongodb-mongosh-org -y &>>$script_file
+
+validate $? "installation of mongo client"
+
+status=$(mongosh --host mongodb.jeev.shop --eval 'db.getMongo().getDBNames().indexof("catalogue")')
+
+if [ $status -lt 0 ]
+then 
+	mongosh --host mongodb.jeev.shop </app/db/master-data.js &>>$script_file
+	validate $? "loading catalogue data to mongoDB"
+else
+	echo -e "Data is already loaded"
+
+fi
+
+print_time
+
+echo -e "$g catalogue setup is completed and script execution completed $n" &>>$script_file
